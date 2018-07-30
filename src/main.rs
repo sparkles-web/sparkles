d9894e1e18d1c49e54040b57cbab5c514c7ca0b8
steveklabnik <steve@steveklabnik.com>
2018-07-30 13:10:30 -0400

    some cleanup

--- src/main.rs
+++ src/main.rs
@@ -7,35 +7,34 @@ use simple_server::{Method, StatusCode};
 
 struct Server;
 
-async fn hello(server: &Server, mut response: simple_server::Builder) -> simple_server::ResponseResult {
-    Ok(response.body("<h1>Hi!</h1><p>Hello Rust!</p>".as_bytes().to_vec()).unwrap())
-}
-
-async fn four_oh_four(server: &Server, mut response: simple_server::Builder) -> simple_server::ResponseResult {
-    response.status(StatusCode::NOT_FOUND);
-    Ok(response.body("<h1>404</h1><p>Not found!<p>".as_bytes().to_vec())?)
-}
-
 impl Server {
+    async fn hello(&self, mut response: simple_server::Builder) -> simple_server::ResponseResult {
+        Ok(response.body("<h1>Hi!</h1><p>Hello Rust!</p>".as_bytes().to_vec()).unwrap())
+    }
+
+    async fn four_oh_four(&self, mut response: simple_server::Builder) -> simple_server::ResponseResult {
+        response.status(StatusCode::NOT_FOUND);
+        Ok(response.body("<h1>404</h1><p>Not found!<p>".as_bytes().to_vec())?)
+    }
+
     fn listen(self, host: &str, port: &str) -> ! {
         let server = simple_server::Server::new(move |request, response| {
             println!("Request received. {} {}", request.method(), request.uri());
 
-            futures::executor::block_on(route(&self, request, response))
+            // futures::executor::block_on(route(&self, request, response))
+            futures::executor::block_on(async {
+                match (request.method(), request.uri().path()) {
+                    (&Method::GET, "/hello") => {
+                        await!(self.hello(response))
+                    }
+                    (_, _) => {
+                        await!(self.four_oh_four(response))
+                    }
+                }
+            })
         });
 
         server.listen(host, port);
-    }
-}
-
-async fn route(server: &Server, request: simple_server::Request<std::vec::Vec<u8>>, response: simple_server::Builder) -> simple_server::ResponseResult {
-    match (request.method(), request.uri().path()) {
-        (&Method::GET, "/hello") => {
-            await!(hello(server, response))
-        }
-        (_, _) => {
-            await!(four_oh_four(server, response))
-        }
     }
 }
 
